b8f8dc71cbda49c8432cb0f981670d828603a3a6
Brian Kozub <ticet11@gmail.com>
2020-05-13 15:48:38 -0600

    Created portfolio-container and integrated it in app.js.

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from './portfolio-container';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>BK React Portfolio</h1>
         <h2>Very good portfolio, thanks.</h2>
+        <PortfolioContainer />
         <div>
           {moment().format('MMMM Do YYYY, h:mm:ss a')}
         </div>
---
+++ src/components/portfolio-container.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class PortfolioContainer extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Portfolio Items here, please.</h2>
+            </div>
+        );
+    }
+}
