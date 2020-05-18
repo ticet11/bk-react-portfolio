272631e70fc26aa432f429687ed0283ce632165b
Brian Kozub <ticet11@gmail.com>
2020-05-18 11:30:48 -0600

    How to Build a Data Filter in React

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -9,11 +9,25 @@ export default class PortfolioContainer extends Component {
         this.state = {
             pageTitle: "Welcome to my portfolio. Do you love it?",
             data: [
-                { title: "Brian", url: "https://briankozub.com" },
-                { title: "Daisy", url: "https://daisykozub.com" },
-                { title: "Sage", url: "https://sagekozub.com" },
+                {
+                    title: "Brian",
+                    url: "https://briankozub.com",
+                    category: "self",
+                },
+                {
+                    title: "Daisy",
+                    url: "https://daisykozub.com",
+                    category: "daughter",
+                },
+                {
+                    title: "Sage",
+                    url: "https://sagekozub.com",
+                    category: "wife",
+                },
             ],
         };
+
+        this.handleFilter = this.handleFilter.bind(this);
     }
 
     portfolioItems() {
@@ -22,10 +36,28 @@ export default class PortfolioContainer extends Component {
         ));
     }
 
+    handleFilter(filter) {
+        this.setState({
+            data: this.state.data.filter((item) => {
+                return item.category === filter;
+            }),
+        });
+    }
+
     render() {
         return (
             <div>
                 <h2>{this.state.pageTitle}</h2>
+
+                <button onClick={() => this.handleFilter("self")}>
+                    Self
+                </button>
+                <button onClick={() => this.handleFilter("wife")}>
+                    Wife
+                </button>
+                <button onClick={() => this.handleFilter("daughter")}>
+                    Daughter
+                </button>
 
                 {this.portfolioItems()}
             </div>
