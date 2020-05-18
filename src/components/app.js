c82dc48bef9a0c705c9194b1c73a9b012e13dd0a
Brian Kozub <ticet11@gmail.com>
2020-05-18 11:58:58 -0600

    How to Use Ternary Operators in JSX Code to Implement Conditional Behavior

--- src/components/app.js
+++ src/components/app.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import moment from "moment";
 
 import PortfolioContainer from './portfolio/portfolio-container';
+import NavigationContainer from './navigation/navigation-container';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <NavigationContainer />
         <h1>BK React Portfolio</h1>
         <h2>Very good portfolio, thanks.</h2>
         <PortfolioContainer />
