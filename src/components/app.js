e98912f9e7987007e23f413bf77dc169836b617f
Brian Kozub <ticet11@gmail.com>
2020-05-18 13:34:00 -0600

    Basic Route Setup in React

--- src/components/app.js
+++ src/components/app.js
@@ -1,21 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import moment from "moment";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
 
-import PortfolioContainer from './portfolio/portfolio-container';
-import NavigationContainer from './navigation/navigation-container';
+import Home from './pages/home';
+import About from './pages/about';
+import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationContainer from "./navigation/navigation-container";
 
 export default class App extends Component {
-  render() {
-    return (
-      <div className='app'>
-        <NavigationContainer />
-        <h1>BK React Portfolio</h1>
-        <h2>Very good portfolio, thanks.</h2>
-        <PortfolioContainer />
-        <div>
-          {moment().format('MMMM Do YYYY, h:mm:ss a')}
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="app">
+                
+
+                <Router>
+                  <div>
+                  <NavigationContainer />
+
+                  <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/about" component={About} />
+                  </Switch>
+                  </div>
+                </Router>
+
+                <h1>BK React Portfolio</h1>
+                <h2>Very good portfolio, thanks.</h2>
+                <PortfolioContainer />
+                <div>
+                    {moment().format("MMMM Do YYYY, h:mm:ss a")}
+                </div>
+            </div>
+        );
+    }
 }
