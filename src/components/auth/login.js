ec7d940bc32b50d9717a171d45ad2b1af9c5fdfc
Brian Kozub <ticet11@gmail.com>
2020-05-20 16:50:57 -0600

    Introduction to React Forms

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,18 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: "",
+            password: "",
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        console.log("handle submit", event);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    }
+
     render() {
         return (
             <div>
                 <h1>Log in here, me.</h1>
-                <form action="">
-                    <input type="text" name="" id=""/>
-                    <input type="password" name="" id=""/>
-                    <button action="Submit">Login</button>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="e-mail"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+                    <div>
+                        <button type="submit">Login</button>
+                    </div>
                 </form>
             </div>
         );
     }
 }
-
-
