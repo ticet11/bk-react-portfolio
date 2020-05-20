e2fbac3b14047647dca8799b7ffaff87efe1bf18
Brian Kozub <ticet11@gmail.com>
2020-05-20 16:56:02 -0600

    Overview of React's Form Submit Handler

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -14,7 +14,7 @@ export default class Login extends Component {
     }
 
     handleSubmit(event) {
-        console.log("handle submit", event);
+        event.preventDefault();
     }
 
     handleChange(event) {
