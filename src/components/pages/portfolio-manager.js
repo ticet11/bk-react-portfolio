2cc8c6a1fe7ff7f18fc69d57a079c6a911bad1ad
Brian Kozub <ticet11@gmail.com>
2020-05-22 13:24:32 -0600

    Portfolio Manager Grid Layout Implementation

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -3,8 +3,15 @@ import React, { Component } from "react";
 export default class PorfolioManager extends Component {
     render() {
         return (
-            <div>
-                <h1>Portfolio Management</h1>
+            <div className='portfolio-manager-wrapper'>
+                <div className="left-column">
+                    <form action="">
+                        <input type="text" placeholder='Portfolio Form' />
+                    </form>
+                </div>
+                <div className="right-column">
+                    <p>Portfolio Sidebar</p>
+                </div>
             </div>
         );
     }
