15a6feabfda233ad4cb311ebe528080b0f98377f
Brian Kozub <ticet11@gmail.com>
2020-05-26 15:26:27 -0600

    Creating a Form Input Mixin

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -149,86 +149,78 @@ export default class PortfolioForm extends Component {
 
     render() {
         return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <input
-                            type="text"
-                            name="name"
-                            placeholder="Item Name"
-                            value={this.state.name}
-                            onChange={this.handleChange}
-                        />
+            <form onSubmit={this.handleSubmit} className='portfolio-form-wrapper'>
+                <div>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Item Name"
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                    />
 
-                        <input
-                            type="text"
-                            name="url"
-                            placeholder="URL"
-                            value={this.state.url}
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="text"
-                            name="position"
-                            placeholder="Position"
-                            value={this.state.position}
-                            onChange={this.handleChange}
-                        />
+                    <input
+                        type="text"
+                        name="url"
+                        placeholder="URL"
+                        value={this.state.url}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    <input
+                        type="text"
+                        name="position"
+                        placeholder="Position"
+                        value={this.state.position}
+                        onChange={this.handleChange}
+                    />
 
-                        <select
-                            name="category"
-                            value={this.state.category}
-                            onChange={this.handleChange}
-                        >
-                            <option value="eCommerce">
-                                eCommerce
-                            </option>
-                            <option value="Scheduling">
-                                Scheduling
-                            </option>
-                            <option value="Enterprise">
-                                Enterprise
-                            </option>
-                        </select>
-                    </div>
-                    <div>
-                        <textarea
-                            type="text"
-                            name="description"
-                            placeholder="Description"
-                            value={this.state.description}
-                            onChange={this.handleChange}
-                        />
-                    </div>
+                    <select
+                        name="category"
+                        value={this.state.category}
+                        onChange={this.handleChange}
+                    >
+                        <option value="eCommerce">eCommerce</option>
+                        <option value="Scheduling">Scheduling</option>
+                        <option value="Enterprise">Enterprise</option>
+                    </select>
+                </div>
+                <div>
+                    <textarea
+                        type="text"
+                        name="description"
+                        placeholder="Description"
+                        value={this.state.description}
+                        onChange={this.handleChange}
+                    />
+                </div>
 
-                    <div className="image-uploaders">
-                        <DropzoneComponent
-                            ref={this.thumbRef}
-                            config={this.compontConfig()}
-                            djsConfig={this.djsConfig()}
-                            eventHandlers={this.handleThumbDrop()}
-                        />
-                        <DropzoneComponent
-                            ref={this.bannerRef}
-                            config={this.compontConfig()}
-                            djsConfig={this.djsConfig()}
-                            eventHandlers={this.handleBannerDrop()}
-                        />
-                        <DropzoneComponent
-                            ref={this.logoRef}
-                            config={this.compontConfig()}
-                            djsConfig={this.djsConfig()}
-                            eventHandlers={this.handleLogoDrop()}
-                        />
-                    </div>
+                <div className="image-uploaders">
+                    <DropzoneComponent
+                        ref={this.thumbRef}
+                        config={this.compontConfig()}
+                        djsConfig={this.djsConfig()}
+                        eventHandlers={this.handleThumbDrop()}
+                    />
+                    <DropzoneComponent
+                        ref={this.bannerRef}
+                        config={this.compontConfig()}
+                        djsConfig={this.djsConfig()}
+                        eventHandlers={this.handleBannerDrop()}
+                    />
+                    <DropzoneComponent
+                        ref={this.logoRef}
+                        config={this.compontConfig()}
+                        djsConfig={this.djsConfig()}
+                        eventHandlers={this.handleLogoDrop()}
+                    />
+                </div>
 
-                    <div>
-                        <button type="submit">Save</button>
-                    </div>
-                </form>
-            </div>
+                <div>
+                    <button type="submit">Save</button>
+                </div>
+            </form>
         );
     }
 }
