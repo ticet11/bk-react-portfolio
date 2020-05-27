28718e0a0ba31e5c942a7c2f2bb3f45288ba1968
Brian Kozub <ticet11@gmail.com>
2020-05-27 13:50:32 -0600

    How to Style Font Awesome Icons in React

--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -1,31 +1,35 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const PortfolioSidebarList = (props) => {
-    const portfolioList = props.data.map(portfolioItem => {
+    const portfolioList = props.data.map((portfolioItem) => {
         return (
-            
-            <div key={portfolioItem.id} className='portfolio-item-thumb'>
-                <div className='portfolio-item-thumb-img'>
-                    <img src={portfolioItem.thumb_image_url} alt=""/>
+            <div
+                key={portfolioItem.id}
+                className="portfolio-item-thumb"
+            >
+                <div className="portfolio-item-thumb-img">
+                    <img src={portfolioItem.thumb_image_url} alt="" />
                 </div>
-                <h1 className='title'>
-                    {portfolioItem.name}
-                </h1>
-                <h2>
-                    {portfolioItem.id}
-                </h2>
-                <a onClick={() => props.handleDeleteClick(portfolioItem)} >
-                    <FontAwesomeIcon icon='trash' />
-                </a>
+                <div className="text-content">
+                    <div className="title">{portfolioItem.name}</div>
+                    <a
+                        className="delete-icon"
+                        onClick={() =>
+                            props.handleDeleteClick(portfolioItem)
+                        }
+                    >
+                        <FontAwesomeIcon icon="trash" />
+                    </a>
+                </div>
             </div>
-        )
-    })
+        );
+    });
     return (
         <div className="portfolio-sidebar-list-wrapper">
             {portfolioList}
         </div>
     );
-}
+};
 
 export default PortfolioSidebarList;
