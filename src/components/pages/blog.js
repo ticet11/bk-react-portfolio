4d7e76b59be0e812820e27807aaed529094f137b
Brian Kozub <ticet11@gmail.com>
2020-05-28 14:35:40 -0600

    Calling the blog API successful.

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-export default function Blog() {
-    return (
-        <div>
-            <h1>Blog</h1>
+export default class Blog extends Component {
+    constructor() {
+        super();
+        this.state = {
+            isLoading: false,
+            data: [],
+        };
+
+        this.getBlogItems = this.getBlogItems.bind(this);
+    }
+
+    getBlogItems() {
+        axios
+            .get(
+                "https://brikozub.devcamp.space/portfolio/portfolio_blogs",
+                {
+                    withCredentials: true,
+                }                  
+            )
+            .then((response) => {
+                this.setState({
+                    data: response.data.portfolio_blogs,
+                });
+            })
+            .catch((error) => {
+                console.error("get blog error", error);
+            });
+    }
+
+    blogItems() {
+        return this.state.data.map((item) => {
+            
+        });
+    }
+
+    componentWillMount() {
+        this.getBlogItems();
+    }
+
+    render() {
+        return (
             <div>
-                <Link to='/about'>More info about me!</Link>
+                <h1>Blog</h1>
+                <div>
+                    <Link to="/about">More info about me!</Link>
+                    {this.blogItems()}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
