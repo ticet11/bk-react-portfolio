7d128fdc8caaac46c8faab0298f9b18d45e3d94c
Brian Kozub <ticet11@gmail.com>
2020-05-29 16:35:46 -0600

    Applying Custom Styles to the React Modal

--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -4,6 +4,20 @@ import ReactModal from "react-modal";
 export default class BlogModal extends Component {
     constructor(props) {
         super(props);
+
+        this.customStyles = {
+            content: {
+                top: "50%",
+                left: "50%",
+                right: "auto",
+                marginRight: "-50%",
+                transform: "translate(-50%, -50%)",
+                width: "800px",
+            },
+            overlay: {
+                backgroundColor: "rgba(1, 1, 1, 0.75)",
+            },
+        };
     }
     render() {
         return (
@@ -13,6 +27,7 @@ export default class BlogModal extends Component {
                     onRequestClose={() => {
                         this.props.handleModalClose();
                     }}
+                    style={this.customStyles}
                 >
                     <h1>ModalBoy</h1>
                 </ReactModal>
