5350a8b37a2eae313285f259c94321d6a9fdf0ec
Brian Kozub <ticet11@gmail.com>
2020-05-30 15:15:46 -0600

    Building the Blog Form Event Handlers

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,16 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class BlogForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: "",
+            blog_status: "",
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        this.props.handleSuccessfulFormSubmission(this.state);
+        event.preventDefault();
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    }
+
     render() {
         return (
-            <form action="">
-                <input type="text"/>
-                <input type="text"/>
-                <input type="text"/>
-                <input type="text"/>
+            <form onSubmit={this.handleSubmit}>
+                <input
+                    type="text"
+                    name="title"
+                    placeholder="Title"
+                    value={this.state.title}
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="text"
+                    name="blog_status"
+                    placeholder="Draft/Published"
+                    value={this.state.blog_status}
+                    onChange={this.handleChange}
+                />
+                {/* <input type="text" />
+                <input type="text" /> */}
 
                 <button>save</button>
             </form>
-        )
+        );
     }
 }
--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -22,7 +22,16 @@ export default class BlogModal extends Component {
                 backgroundColor: "rgba(1, 1, 1, 0.75)",
             },
         };
+
+        this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(
+            this
+        );
     }
+
+    handleSuccessfulFormSubmission(blog) {
+        console.log("Bob Log's Law Blog", blog);
+    }
+
     render() {
         return (
             <div>
@@ -33,7 +42,7 @@ export default class BlogModal extends Component {
                     }}
                     style={this.customStyles}
                 >
-                    <BlogForm />
+                    <BlogForm handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission} />
                 </ReactModal>
             </div>
         );
