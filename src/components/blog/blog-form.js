a1e10f08bdfb87261ff9a048c1993a5a43cd0adb
Brian Kozub <ticet11@gmail.com>
2020-06-01 19:47:22 -0600

    Configuring Dropzone for Blog Featured Image

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Axios from "axios";
+import DropzoneComponent from "react-dropzone-component";
 
 import RichTextEditor from "../forms/rich-text-editor";
 
@@ -11,12 +12,41 @@ export default class BlogForm extends Component {
             title: "",
             blog_status: "",
             content: "",
+            featured_image: "",
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleRichTextEditorChange = this.handleRichTextEditorChange.bind(
             this
         );
+
+        this.componentConfig = this.componentConfig.bind(this);
+        this.djsConfig = this.djsConfig.bind(this);
+        this.handleFeaturedImageDrop = this.handleFeaturedImageDrop.bind(
+            this
+        );
+    }
+
+    componentConfig() {
+        return {
+            iconFiletypes: [".jpg", ".png"],
+            showFiletypeIcon: true,
+            postUrl: "https://httpbin.org/post",
+        };
+    }
+
+    djsConfig() {
+        return {
+            addRemoveLinks: true,
+            maxFiles: 1,
+        };
+    }
+
+    handleFeaturedImageDrop() {
+        return {
+            addedfile: (file) =>
+                this.setState({ featured_image: file }),
+        };
     }
 
     handleRichTextEditorChange(content) {
@@ -96,6 +126,18 @@ export default class BlogForm extends Component {
                             this.handleRichTextEditorChange
                         }
                     />
+                </div>
+
+                <div className="image-uploaders">
+                    <DropzoneComponent
+                        config={this.componentConfig()}
+                        djsConfig={this.djsConfig()}
+                        eventHandlers={this.handleFeaturedImageDrop()}
+                    >
+                        <div className="dz-message">
+                            Featured Image
+                        </div>
+                    </DropzoneComponent>
                 </div>
 
                 <button className="btn">save</button>
