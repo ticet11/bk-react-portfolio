6af2eb71947988074a919ef20ecadf018771f12e
Brian Kozub <ticet11@gmail.com>
2020-06-02 16:56:43 -0600

    Building About Page Styles for React Portfolio

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
     return (
         <div>
-            <h1>About</h1>
+            <div className="content-page-wrapper two-column">
+                <div
+                    className="left-column"
+                    style={{
+                        background:
+                            'url("https://www.thewrap.com/wp-content/uploads/2019/08/Guy-Fieri.jpg") no-repeat',
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                    }}
+                >
+                    {/* <img
+                        className="bio-image"
+                        src="https://www.thewrap.com/wp-content/uploads/2019/08/Guy-Fieri.jpg"
+                        alt=""
+                    /> */}
+                </div>
+                <div className="right-column bio-content">
+                    Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit. Dicta ipsa molestias sit maiores earum
+                    repudiandae vitae atque omnis voluptatem nisi cum
+                    itaque, placeat autem ab vero excepturi optio
+                    commodi veritatis! Corporis harum illo veniam
+                    sapiente consectetur! Ipsa a dolorum ex quod
+                    accusamus error nostrum consequuntur aliquid id ad
+                    quisquam, placeat eaque. Vero, iure! Delectus
+                    doloremque, nam est recusandae ipsum dicta! Alias
+                    iste pariatur eius numquam est magnam rem magni
+                    aut, optio harum ullam repellat excepturi ab animi
+                    consequuntur neque quis nisi iusto quidem et
+                    officia placeat nulla facere. Libero, magnam!
+                    Reprehenderit unde illo enim inventore non dolorem
+                    doloremque rerum libero suscipit iusto quos ea
+                    mollitia vero quibusdam esse provident aut, vitae
+                    soluta deleniti blanditiis reiciendis sunt qui.
+                </div>
+            </div>
         </div>
-    )
+    );
 }
