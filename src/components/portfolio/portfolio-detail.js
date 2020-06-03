fdd5358058c75a749bc578f26fe859be29d9a0d2
Brian Kozub <ticet11@gmail.com>
2020-06-02 21:29:17 -0600

    Initial Build Out of the Portfolio Detail Component

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,9 +1,50 @@
 import React from "react";
+import axios from "axios";
 
-export default function PortfolioDetail(props) {
-    return (
-        <div>
-            <h2>Portfolio Details for {props.match.params.slug}</h2>
-        </div>
-    );
+export default class PortfolioDetail extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            portfolioItem: {},
+        };
+    }
+
+    UNSAFE_componentWillMount = () => {
+        this.getPortfolioItemDetails();
+    };
+
+    getPortfolioItemDetails = () => {
+        axios
+            .get(
+                `https://brikozub.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`
+            )
+            .then((response) => {
+                console.log(response);
+                this.setState({
+                    portfolioItem: response.data.portfolio_item,
+                });
+            })
+            .catch((error) => {
+                console.error("getPortfolioItemDetails error", error);
+            });
+    };
+
+    render() {
+        const {
+            id,
+            banner_image_url,
+            thumb_image_url,
+            logo_url,
+            category,
+            description,
+            name,
+            url,
+        } = this.state.portfolioItem;
+        return (
+            <div>
+                <h2>{name}</h2>
+            </div>
+        );
+    }
 }
--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class PortfolioItem extends Component {
     constructor(props) {
@@ -24,24 +25,26 @@ export default class PortfolioItem extends Component {
             logo_url,
         } = this.props.item;
         return (
-            <div
-                className="portfolio-item-wrapper"
-                onMouseEnter={() => this.handleMouseEnter()}
-                onMouseLeave={() => this.handleMouseLeave()}
-            >
+            <Link to={`/portfolio/${id}`} >
                 <div
-                    className={`portfolio-img-background ${this.state.portfolioItemClass}`}
-                    style={{
-                        backgroundImage: `url(${thumb_image_url})`,
-                    }}
-                />
-                <div className="img-text-wrapper">
-                    <div className="logo-wrapper">
-                        <img src={logo_url} alt="" />
+                    className="portfolio-item-wrapper"
+                    onMouseEnter={() => this.handleMouseEnter()}
+                    onMouseLeave={() => this.handleMouseLeave()}
+                >
+                    <div
+                        className={`portfolio-img-background ${this.state.portfolioItemClass}`}
+                        style={{
+                            backgroundImage: `url(${thumb_image_url})`,
+                        }}
+                    />
+                    <div className="img-text-wrapper">
+                        <div className="logo-wrapper">
+                            <img src={logo_url} alt="" />
+                        </div>
+                        <div className="subtitle">{description}</div>
                     </div>
-                    <div className="subtitle">{description}</div>
                 </div>
-            </div>
+            </Link>
         );
     }
 }
