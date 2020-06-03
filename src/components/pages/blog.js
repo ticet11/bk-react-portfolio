7c00055a3912a1c989cc3df17a69ebb8756a967c
Brian Kozub <ticet11@gmail.com>
2020-06-03 10:04:05 -0600

    Enabling the Ability to Delete Blog Posts

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -24,6 +24,28 @@ export default class Blog extends Component {
         this.handleSuccessfulNewBlogSubmission = this.handleSuccessfulNewBlogSubmission.bind(
             this
         );
+        this.handleDeleteClick = this.handleDeleteClick.bind(this);
+    }
+
+    handleDeleteClick(blog) {
+        axios
+            .delete(
+                `https://api.devcamp.space/portfolio/portfolio_blogs/${blog.id}`,
+                { withCredentials: true }
+            )
+            .then((response) => {
+                this.setState({
+                    data: this.state.data.filter(
+                        (blogItem) => {
+                            return blog.id !== blogItem.id;
+                        }
+                    ),
+                });
+                return response.data;
+            })
+            .catch((error) => {
+                console.error("handle delete error", error);
+            });
     }
 
     handleSuccessfulNewBlogSubmission(blog) {
@@ -96,7 +118,27 @@ export default class Blog extends Component {
 
     render() {
         const blogRecords = this.state.data.map((blogItem) => {
-            return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+            if (this.props.loggedInStatus === "LOGGED_IN") {
+                return (
+                    <div
+                        key={blogItem.id}
+                        className="admin-blog-wrapper"
+                    >
+                        <BlogItem blogItem={blogItem} />
+                        <a
+                            onClick={() =>
+                                this.handleDeleteClick(blogItem)
+                            }
+                        >
+                           <FontAwesomeIcon icon='trash' />
+                        </a>
+                    </div>
+                );
+            } else {
+                return (
+                    <BlogItem key={blogItem.id} blogItem={blogItem} />
+                );
+            }
         });
 
         return (
