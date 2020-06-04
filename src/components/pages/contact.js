2277a90de5cb53e46a4d58f457b8612c42b8307f
Brian Kozub <ticet11@gmail.com>
2020-06-03 20:54:01 -0600

    updated contact page links and styling.

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -18,52 +18,97 @@ export default function Contact() {
                 <div className="contact-bullet-points one-column">
                     <div className="bullet-point-group">
                         <div className="icon">
-                            <a href="https://www.linkedin.com/in/briankozub">
+                            <a
+                                target="_blank"
+                                href="https://www.linkedin.com/in/briankozub"
+                            >
                                 <FontAwesomeIcon
                                     icon={["fab", "linkedin"]}
                                 />
                             </a>
                         </div>
                         <div className="text">
-                            LinkedIn.com/in/briankozub
+                            <a
+                                target="_blank"
+                                href="https://www.linkedin.com/in/briankozub"
+                            >
+                                linkedin.com/in/briankozub
+                            </a>
                         </div>
                     </div>
                     <div className="bullet-point-group">
                         <div className="icon">
-                            <a href="https://www.github.com/ticet11">
+                            <a
+                                target="_blank"
+                                href="https://www.github.com/ticet11"
+                            >
                                 <FontAwesomeIcon
                                     icon={["fab", "github"]}
                                 />
                             </a>
                         </div>
-                        <div className="text">github.com/ticet11</div>
+                        <div className="text">
+                            <a
+                                target="_blank"
+                                href="https://www.github.com/ticet11"
+                            >
+                                github.com/ticet11
+                            </a>
+                        </div>
                     </div>
                     <div className="bullet-point-group">
                         <div className="icon">
-                            <a href="mailto:brian@briankozub.com">
+                            <a
+                                target="_blank"
+                                href="mailto:brian@briankozub.com"
+                            >
                                 <FontAwesomeIcon icon="paper-plane" />
                             </a>
                         </div>
                         <div className="text">
-                            brian@briankozub.com
+                            <a
+                                target="_blank"
+                                href="mailto:brian@briankozub.com"
+                            >
+                                brian@briankozub.com
+                            </a>
                         </div>
                     </div>
                     <div className="bullet-point-group">
                         <div className="icon">
-                            <a href="tel:385-722-0126">
+                            <a
+                                target="_blank"
+                                href="tel:385-722-0126"
+                            >
                                 <FontAwesomeIcon icon="mobile-alt" />
                             </a>
                         </div>
-                        <div className="text">(385) 722-0126</div>
+                        <div className="text">
+                            <a
+                                target="_blank"
+                                href="tel:385-722-0126"
+                            >
+                                (385) 722-0126
+                            </a>
+                        </div>
                     </div>
                     <div className="bullet-point-group">
                         <div className="icon">
-                            <a href="#">
+                            <a
+                                target="_blank"
+                                href="https://goo.gl/maps/Dqy7sq9wEpd7erJ16"
+                            >
                                 <FontAwesomeIcon icon="map-marked-alt" />
                             </a>
                         </div>
                         <div className="text">
-                            Sandy, UT (Open to remote opportunities)
+                            <a
+                                target="_blank"
+                                href="https://goo.gl/maps/Dqy7sq9wEpd7erJ16"
+                            >
+                                Sandy, UT (Open to remote
+                                opportunities)
+                            </a>
                         </div>
                     </div>
                 </div>
