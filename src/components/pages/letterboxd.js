78446283449baad27e74f61e8e54f0eabae1cafa
Brian Kozub <ticet11@gmail.com>
2020-07-26 14:24:09 -0600

    basic functionality of letterboxd page.

--- src/components/pages/letterboxd.js
+++ src/components/pages/letterboxd.js
@@ -1,14 +1,26 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Letterboxd = () => {
-    
+    const [feed, setFeed] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const retrievedFeed = parser.parseURL(
+            "https://letterboxd.com/ticet11/rss/"
+        );
+        setFeed(retrievedFeed);
+    });
 
     return (
         <div>
-            
+            {feed.map((item, index) => (
+                <div key={i}>
+                    <h1>item.title</h1>
+                </div>
+            ))}
         </div>
     );
-}
+};
 
 export default letterboxd;
