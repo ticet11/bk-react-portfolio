d33d584dfaeb20a4c10ceda495058bbca6f25d5b
Brian Kozub <ticet11@gmail.com>
2020-07-26 22:40:09 -0600

    letterboxd feed renders properly on screen

--- src/components/pages/letterboxd.js
+++ src/components/pages/letterboxd.js
@@ -1,28 +1,48 @@
 import React, { useState, useEffect } from "react";
+import RSSParser from "rss-parser";
 
 const Letterboxd = () => {
     const [feed, setFeed] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
-    const parser = new DOMParser();
+    const parser = new RSSParser();
 
     useEffect(() => {
-        fetch(
-            "https://cors-anywhere.herokuapp.com/https://letterboxd.com/ticet11/rss/"
-        )
-            .then((res) => res.text())
-            .then((str) => parser.parseFromString(str, "text/xml"))
-            .then((data) => {
-                console.log(data.querySelectorAll('item'))
-                setFeed(data.querySelectorAll('item'));
-            });
+        parser.parseURL(
+            "https://cors-anywhere.herokuapp.com/https://letterboxd.com/ticet11/rss/",
+            function (err, feed) {
+                if (err) throw err;
+                setFeed(feed.items);
+            }
+        );
         if (feed !== []) {
             setIsLoading(false);
         }
         return () => "finished";
     }, []);
 
-    return <div>{isLoading ? "Loading..." : "Not Loading"}</div>;
+    const createMarkup = (contentText) => {
+        return {__html: contentText}
+    }
+
+    return (
+        <div>
+            {isLoading
+                ? "Loading..."
+                : feed.map((item) => {
+                      console.log(item);
+                      return (
+                          <div
+                              key={item.isoDate}
+                              className="movie-card"
+                          >
+                              <h2>{item.title}</h2>
+                              <div className='description' dangerouslySetInnerHTML={createMarkup(item.content)}></div>
+                          </div>
+                      );
+                  })}
+        </div>
+    );
 };
 
 export default Letterboxd;
