f620249eed2859dffb1d4243c52254ec286125b6
Brian Kozub <ticet11@gmail.com>
2020-07-26 21:32:33 -0600

    able to fetch rss feed and setFeed as an array of items.

--- src/components/pages/letterboxd.js
+++ src/components/pages/letterboxd.js
@@ -1,26 +1,44 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import Parser from "rss-parser";
 
 const Letterboxd = () => {
     const [feed, setFeed] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        const retrievedFeed = parser.parseURL(
-            "https://letterboxd.com/ticet11/rss/"
-        );
-        setFeed(retrievedFeed);
-    });
+    const parser = new DOMParser();
 
-    return (
-        <div>
-            {feed.map((item, index) => (
-                <div key={i}>
-                    <h1>item.title</h1>
-                </div>
-            ))}
-        </div>
-    );
+    // useEffect(() => {
+    //     async function fetchFeed() {
+    //         try {
+    //             const retrievedFeed = await parser.parseURL(
+    //                 "https://cors-anywhere.herokuapp.com/https://letterboxd.com/ticet11/rss/"
+    //             );
+    //             setFeed(await retrievedFeed);
+    //         } catch (err) {
+    //             console.error(`fetch rss feed error: ${err}`);
+    //         }
+    //     }
+    //     console.log(feed)
+    //     return () => retrievedFeed;
+    // });
+
+    useEffect(() => {
+        fetch(
+            "https://cors-anywhere.herokuapp.com/https://letterboxd.com/ticet11/rss/"
+        )
+            .then((res) => res.text())
+            .then((str) => parser.parseFromString(str, "text/xml"))
+            .then((data) => {
+                console.log(data.querySelectorAll('item'))
+                setFeed(data.querySelectorAll('item'));
+            });
+        if (feed !== []) {
+            setIsLoading(false);
+        }
+        return () => "finished";
+    }, []);
+
+    return <div>{isLoading ? "Loading..." : "Not Loading"}</div>;
 };
 
 export default Letterboxd;
