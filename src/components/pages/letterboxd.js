4a25223e331decacb4f0e0476fede5196f018659
Brian Kozub <ticet11@gmail.com>
2020-07-26 21:34:12 -0600

    removed unnecessary package and code from letterboxd page.

--- src/components/pages/letterboxd.js
+++ src/components/pages/letterboxd.js
@@ -1,26 +1,10 @@
 import React, { useState, useEffect } from "react";
-import Parser from "rss-parser";
 
 const Letterboxd = () => {
     const [feed, setFeed] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
     const parser = new DOMParser();
-
-    // useEffect(() => {
-    //     async function fetchFeed() {
-    //         try {
-    //             const retrievedFeed = await parser.parseURL(
-    //                 "https://cors-anywhere.herokuapp.com/https://letterboxd.com/ticet11/rss/"
-    //             );
-    //             setFeed(await retrievedFeed);
-    //         } catch (err) {
-    //             console.error(`fetch rss feed error: ${err}`);
-    //         }
-    //     }
-    //     console.log(feed)
-    //     return () => retrievedFeed;
-    // });
 
     useEffect(() => {
         fetch(
