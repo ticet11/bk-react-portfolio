6dda3b24f79526b9a6d20fa601dd2c9db7cfed77
Brian Kozub <ticet11@gmail.com>
2020-07-28 18:47:12 -0600

    separated title and stars for styling.

--- src/components/pages/letterboxd.js
+++ src/components/pages/letterboxd.js
@@ -25,9 +25,27 @@ const Letterboxd = () => {
         return { __html: contentText };
     };
 
+    const starSlicer = (title, method) => {
+        let starIndex = title.indexOf(" - ");
+        console.log(starIndex);
+        // title.indexOf(" - ★") === -1
+        //     ? (starIndex = title.indexOf(" - ½"))
+        //     : (starIndex = -1);
+        if (method === "titleOnly" && starIndex !== -1) {
+            return title.slice(0, starIndex + 1);
+        } else if (method === "titleOnly") {
+            return title.replace(" (contains spoilers)", "");
+        }
+        if (method === "starsOnly" && starIndex !== -1) {
+            return title
+                .slice(starIndex + 3, -1)
+                .replace(" (contains spoilers", "");
+        }
+    };
+
     return (
         <div className="card-container">
-            <h1 className='header'>What Have I Been Watching?</h1>
+            <h1 className="header">What Have I Been Watching?</h1>
             <div className="card-list">
                 {isLoading
                     ? "Loading..."
@@ -39,14 +57,23 @@ const Letterboxd = () => {
                                   className="card"
                               >
                                   <div className="title-wrapper">
-                                      <a href={item.link} target='_blank'>
+                                      <a
+                                          href={item.link}
+                                          target="_blank"
+                                      >
                                           <h2 className="title">
-                                              {item.title.replace(
-                                                  " (contains spoilers)",
-                                                  ""
+                                              {starSlicer(
+                                                  item.title,
+                                                  "titleOnly"
                                               )}
                                           </h2>
                                       </a>
+                                      <p className="star-rating">
+                                          {starSlicer(
+                                              item.title,
+                                              "starsOnly"
+                                          )}
+                                      </p>
                                       <p className="review-date">
                                           Review published:{" "}
                                           <i>
