f56a176b93a6203eb1158b7374434b8cf5eec490
Brian Kozub <ticet11@gmail.com>
2021-09-23 17:02:35 -0600

    Updated all dependencies that didn't claim to have breaking changes. Updated webpack and fixed breaking changes. Project seems to start and run as expected.

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const webpackMerge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const webpackCommon = require('./common.config');
 
 const env = require('../env');
@@ -8,87 +8,84 @@ const proxyRules = require('../proxy/rules');
 // webpack plugins
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const DefinePlugin = require('webpack/lib/DefinePlugin');
-const HotModuleReplacementPlugin = require('webpack/lib/HotModuleReplacementPlugin');
 
-module.exports = webpackMerge(webpackCommon, {
+module.exports = merge(webpackCommon, {
+	devtool: 'inline-source-map',
+	mode: 'development',
+	output: {
+		path: path.resolve(__dirname, '../static/dist'),
 
-  devtool: 'inline-source-map',
-  mode: 'development',
-  output: {
-  
-    path: path.resolve(__dirname, '../static/dist'),
+		filename: '[name].js',
 
-    filename: '[name].js',
+		sourceMapFilename: '[name].map',
 
-    sourceMapFilename: '[name].map',
+		chunkFilename: '[id]-chunk.js',
 
-    chunkFilename: '[id]-chunk.js',
+		publicPath: '/',
+	},
 
-    publicPath: '/'
+	module: {
+		rules: [
+			{
+				test: /\.s?css$/,
+				use: [
+					{
+						loader: 'style-loader',
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 2,
+						},
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							outputStyle: 'expanded',
+							sourceMap: true,
+							sourceMapContents: true,
+						},
+					},
+				],
+			},
+		],
+	},
 
-  },
+	plugins: [
+		new DefinePlugin({
+			'process.env': {
+				NODE_ENV: "'development'",
+			},
+		}),
+		new HtmlWebpackPlugin({
+			inject: true,
+			template: path.resolve(__dirname, '../static/index.html'),
+			favicon: path.resolve(__dirname, '../static/favicon.ico'),
+		}),
+	],
 
-  module: {
-
-    rules: [
-      {
-        test: /\.s?css$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 2
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              outputStyle: 'expanded',
-              sourceMap: true,
-              sourceMapContents: true
-            }
-          }
-        ]
-      }
-    ]
-
-  },
-
-  plugins: [
-    new DefinePlugin({
-      'process.env': {
-        NODE_ENV: "'development'"
-      }
-    }),
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: path.resolve(__dirname, '../static/index.html'),
-      favicon: path.resolve(__dirname, '../static/favicon.ico')
-    }),
-    new HotModuleReplacementPlugin()
-  ],
-
-  devServer: {
-    host: env.devServer.host || 'localhost',
-    port: env.devServer.port || 3000,
-    contentBase: path.resolve(__dirname, '../static'),
-    watchContentBase: true,
-    compress: true,
-    hot: true,
-    historyApiFallback: {
-      disableDotRule: true
-    },
-    watchOptions: {
-      ignored: /node_modules/
-    },
-    overlay: {
-      warnings: true,
-      errors: true
-    },
-    proxy: proxyRules
-  }
-
+	devServer: {
+		host: env.devServer.host || 'localhost',
+		port: env.devServer.port || 3000,
+		static: {
+			staticOptions: {
+				contentBase: path.resolve(__dirname, '../static'),
+				watchContentBase: true,
+				watchOptions: {
+					ignored: /node_modules/,
+				},
+			},
+		},
+		hot: true,
+		historyApiFallback: {
+			disableDotRule: true,
+		},
+		client: {
+			overlay: {
+				warnings: true,
+				errors: true,
+			},
+		},
+		proxy: proxyRules,
+	},
 });
