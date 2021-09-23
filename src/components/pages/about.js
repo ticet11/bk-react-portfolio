db33dcd51065cc1f1c97cd1f8f813cf9ed1797d1
Brian Kozub <ticet11@gmail.com>
2021-09-22 22:41:11 -0600

    Updated about page, README, and .gitignore.
    [.gitignore]
    * Changed line 132 to {.vscode/}
    + Added liens 139-140 {# Prettier
    .prettierignore}
    
    [layouts.scss]
    Style changes so about and contact page shouldn't have to scroll on desktop.
    * Changed line 51 {height: calc(100vh - 85px);
    }
    * Changed line 54 {padding: 20px;
    }

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,56 +1,54 @@
-import React from "react";
+import React from 'react';
 
 export default function About() {
-    return (
-        <div>
-            <div className="content-page-wrapper two-column">
-                <div className="left-column about-img"></div>
-                <div className="right-column bio-content">
-                    <p>
-                        Hi, thanks for coming to take a look at my
-                        work. My name is Brian and I look forward to
-                        getting to know you!
-                    </p>
-                    <p>
-                        My wife and I have a 2 year old little girl.
-                        We also have a cat, named Gatsby, that my wife
-                        likes more than I do. He's okay, I guess.
-                    </p>
-                    <p>
-                        In my spare time, if I’m not coding, I like
-                        fixing cars and motorcycles and watching
-                        movies. Check out what movies I've been
-                        watching on the reviews page.
-                    </p>
-                    <p>
-                        I'm currently working at Avii as an Apprentice
-                        Software Engineer. As a part of my current
-                        project, I'm learning ASP.NET and C#.
-                    </p>
-                    <p>These are some of my other skills:</p>
+	return (
+		<div>
+			<div className="content-page-wrapper two-column">
+				<div className="left-column about-img"></div>
+				<div className="right-column bio-content">
+					<p>
+						Hi, thanks for coming to take a look at my work. My name is Brian and I look
+						forward to getting to know you! I'm originally from Upstate New York. I've
+						been married to my wife, Sage, for 4 years and we've got a 3 year old
+						daughter, Daisy.
+					</p>
+					<p>
+						In my spare time, if I’m not coding, I like fixing cars and motorcycles and
+						watching movies. Check out which movies I've been watching, on the reviews
+						page.
+					</p>
+					<p>
+						Before getting into web development, I worked as a baker, a construction
+						worker, a guide at a floatation therapy center, and a delivery driver. I'd
+						love an opportunity to use some of my previous experiences in a development
+						role.
+					</p>
+					<p>
+						We came back to Utah in 2020 so I could attend Bottega's web development
+						bootcamp, just in time for everything to shut down, so it was all remote.
+						About 2 months after completing my certification, I was able to land an
+						apprenticeship with Avii, where I worked for a year, mainly in updating
+						legacy .NET MVC enterprise products.
+					</p>
+					<p>Some of my skills:</p>
 
-                    <ul className="skills-container">
-                        <li>Databases, including MongoDB and SQL</li>
-                        <li>React.js</li>
-                        <li>Lua and Love2D</li>
-                        <li>HTML, CSS, SCSS</li>
-                        <li>Git, mostly in GitHub</li>
-                        <li>
-                            Linux, including Mint, Ubuntu, and
-                            Raspbian
-                        </li>
-                        <li>Public speaking and leadership</li>
-                        <li>Teaching and training</li>
-                    </ul>
+					<ul className="skills-container">
+						<li>.NET</li>
+						<li>C#</li>
+						<li>React and React Native</li>
+						<li>HTML, CSS, SCSS, JavaScript</li>
+						<li>TSQL</li>
+						<li>Git, in GitHub and Azure DevOps</li>
+					</ul>
 
-                    <p>
-                        I don't know the answer to every question, but
-                        do know how to find those answers and I love
-                        the process. I love learning new skills and
-                        finding new ways to solve problems.
-                    </p>
-                </div>
-            </div>
-        </div>
-    );
+					<p>
+						I don't know everything yet, but I have become an expert at finding answers
+						to my questions. I love to learn and I'm eager to find a team to grow with
+						and contribute to. If I seem like a good fit for a position with your
+						company, my contact info is in the next tab! I hope to hear from you soon.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 }
