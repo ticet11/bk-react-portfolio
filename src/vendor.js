86d6a8c6e3fbe86b36684978082688600082144b
Brian Kozub <ticet11@gmail.com>
2021-09-24 14:30:43 -0600

    Attempt to fix buffer not defined error.

--- src/vendor.js
+++ src/vendor.js
@@ -2,6 +2,9 @@
 
 // polyfills and vendors
 
+window.global = window;
+window.Buffer = window.Buffer || require('buffer').Buffer;
+
 if (!window._babelPolyfill) {
-  require('babel-polyfill');
+	require('babel-polyfill');
 }
